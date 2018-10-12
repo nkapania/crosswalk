4cc6678e367bde4cd2129ce2ddb3a362c954cb41
Nitin <nkapania@gmail.com>
2018-10-12 16:46:54 -0700

    state machine now workinggit status

--- main.py
+++ main.py
@@ -2,17 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 from utils import *
 
-road = Road()
-crosswalk = Crosswalk(road)
-veh = Vehicle()
+
+roadLength = 50
+roadFraction = 0.8
+
+road = Road(length = roadLength)
+crosswalk = Crosswalk(road, roadFraction = roadFraction) #roadfraction controls where the crosswalk starts
+veh = Vehicle(xBrake = roadLength * 0.4, v0 = 15.)
 ped = Pedestrian()
 
-sim = Simulation(road, crosswalk, veh, ped)
+sim = Simulation(road, crosswalk, veh, ped, N = 200, ts = 0.1)
 out = sim.run()
 
 sim.animate()
 
 # plt.figure()
-# plt.plot(out["t"],out["xP"])
+# plt.plot(out["t"],out["dxP"])
 # plt.show()
 
+# plt.figure()
+# plt.plot(out["xV"], out["dxVdes"])
+# plt.plot(out["xV"], out["dxV"])
+# plt.show()
+
+# plt.figure()
+# plt.plot(out["t"], out["vehicleState"])
+# plt.plot(out["t"], out["pedestrianState"])
+# plt.show()
+
+# plt.figure()
+# plt.plot(out["xV"], out["ddxV"])
+# plt.show()
--- utils.py
+++ utils.py
@@ -4,13 +4,13 @@ import matplotlib.patches as patches
 from matplotlib import animation
 
 class Road:
-    def __init__(self):
+    def __init__(self, length = 80):
         
         x0 = 0
         y0 = 0 #start at origin
         self.numLanes = 4 
         self.laneWidth = 3.7 #meters
-        self.length = 80. #meters
+        self.length = length #meters
         self.fig, self.ax = plt.subplots(1)
         self.startIC = (x0,y0)
         self.color = '#949392' #light grey
@@ -27,7 +27,11 @@ class Road:
             x += self.laneWidth
 
             if i < self.numLanes - 1:
-                plt.plot([x, x], [y, y + self.length],color = 'w',LineStyle = '--', LineWidth = 3)
+                if i == 1: #median
+                    plt.plot([x, x], [y, y + self.length],color = 'y',LineStyle = '-', LineWidth = 3)
+                else:
+                    plt.plot([x, x], [y, y + self.length],color = 'w',LineStyle = '--', LineWidth = 3)
+
         
         x0,y0 = self.startIC
         self.ax.axis('equal') 
@@ -35,15 +39,16 @@ class Road:
         self.ax.set_xlim([x - 1, x + self.laneWidth * self.numLanes]) 
         
 class Crosswalk:
-    def __init__(self, road):
+    def __init__(self, road, roadFraction = 0.5):
         self.width = road.numLanes * road.laneWidth
         self.height = 3.0 #meters
         self.fig = road.fig
         self.ax  = road.ax
         self.numBars = road.numLanes * 4 #number of bars on crosswalk for graphical purposes
+        self.roadFraction = roadFraction
 
         x,y = road.startIC
-        self.start = (x, y + road.length * .67)
+        self.start = (x, y + road.length * self.roadFraction)
         self.plot()
 
     def getAxis(self):
@@ -62,33 +67,97 @@ class Crosswalk:
             x1 += 2 * barWidth
 
 class Vehicle:
-    def __init__(self, v0 = 20):
+    #cY = y coordinate of crosswalk
+    def __init__(self, xBrake, v0 = 20):
         self.m = 1500. #kg
         self.v0 = v0
         self.lane = 2 # lane 2 corresponds to third lane from the left
         self.width = 1.5 #meters
         self.height = 2.5 #meters
+        self.state = "driving"
+        self.kSpeed = 3.
+        self.xBrake =  xBrake #position to start braking the car 
 
-    def getAccel(self):
-        accel = 0
+    def getAccel(self, xP, dxP, xV, dxV, t, crosswalk):
+        accel = self.getAccelStateMachine(xP, dxP, xV, dxV, t, crosswalk)
         return accel
 
+    def getAccelStateMachine(self, xP, dxP, xV, dxV, t, crosswalk):
+        if self.state == "driving":
+            dxVdes = self.v0
+            accel = self.kSpeed*(dxVdes - dxV)
+            state = 0
+
+            #check if pedestrian in crosswalk
+            if xP > 0 and xP < crosswalk.width:
+                self.state = "braking"
+
+        elif self.state == "braking":
+            xStop = crosswalk.start[1] - self.height * 2 #give buffer for vehicle stop
+
+            if xV < self.xBrake:
+                dxVdes = self.v0  #before brakepoint - keep driving!
+
+            elif xV > xStop:
+                dxVdes = 0. #past the crosswalk, desired speed is 0
+
+            else:
+                dxVdes = self.v0 - self.v0 / (xStop - self.xBrake) * (xV - self.xBrake)
+            
+
+            accel = self.kSpeed*(dxVdes - dxV)
+            state = 1
+
+            if xP > crosswalk.width:
+                self.state = "driving"
+
+        return accel, state, dxVdes
+
+
+
 class Pedestrian:
-    def __init__(self, v0 = 1.2):
+    def __init__(self, v0 = 0):
         self.m = 50. #kg
         self.v0 = v0
         self.radius = 1. #m
+        self.state = "waiting" #possible states are "waiting"
+        self.waitTime = 0.5 #number of seconds pedestrian takes to gauge situation
+        self.kSpeed = 10 #m/s2 per m/s of error
+        self.vDes = 1.2 #m/s
+        self.minGap = 30. #m
 
-    def getAccel(self):
-        accel = 0
+    def getAccel(self, xP, dxP, xV, t, crosswalk):
+        accel = self.getAccelStateMachine(xP, dxP, xV, t, crosswalk)
         return accel
+
+    def getAccelStateMachine(self, xP, dxP, xV, t, crosswalk):
+        gap = self.calculateGap(xV, crosswalk)
+        if self.state == "waiting":
+            if t > self.waitTime and gap > self.minGap:
+                self.state = "walking"
+
+            state = 0
+            accel = 0
+        elif self.state == "walking":
+            accel = self.kSpeed * (self.vDes - dxP)
+            state = 1
+
+        return accel, state, gap
+
+    def calculateGap(self,xV, crosswalk):
+        gap = crosswalk.start[1] - xV
+        #car is past the crosswalk
+        if gap < 0:
+            gap = 99999.
+
+        return gap
 
 
 #Simulate with double euler integration
 class Simulation:
-    def __init__(self, road, crosswalk, vehicle, pedestrian):
-        self.ts = 0.1 #seconds
-        self.N = 100 #number of time steps to simulate
+    def __init__(self, road, crosswalk, vehicle, pedestrian, ts = 0.1, N = 100):
+        self.ts = ts #seconds
+        self.N = N #number of time steps to simulate
         self.vehicle = vehicle
         self.road = road
         self.pedestrian = pedestrian
@@ -102,12 +171,18 @@ class Simulation:
         #vehicle motion
         xV = np.zeros((self.N, 1))
         dxV = np.zeros(xV.shape); dxV[0] = self.vehicle.v0
-        ddxV = np.zeros(dxV.shape);
+        ddxV = np.zeros(dxV.shape)
 
         #pedestrian motion
         xP = np.zeros((self.N, 1))
         dxP = np.zeros(xP.shape); dxP[0] = self.pedestrian.v0
         ddxP = np.zeros(xP.shape)
+
+        pedestrianState =  np.zeros(xP.shape)
+        vehicleState = np.zeros(xP.shape)
+        gap = np.zeros(xP.shape)
+        dxVdes = np.zeros(xP.shape)
+
 
         #other arrays
         t = np.zeros((self.N, 1))
@@ -117,8 +192,8 @@ class Simulation:
         for i in range(1, self.N):
             t[i] = t[i-1] + self.ts
 
-            ddxV[i] = self.vehicle.getAccel()
-            ddxP[i] = self.pedestrian.getAccel()
+            ddxV[i], vehicleState[i], dxVdes[i] = self.vehicle.getAccel(xP[i-1], dxP[i-1], xV[i-1], dxV[i-1], t[i-1], self.crosswalk)
+            ddxP[i], pedestrianState[i], gap[i] = self.pedestrian.getAccel(xP[i-1], dxP[i-1], xV[i-1], t[i-1], self.crosswalk)
 
             dxV[i] = self.ts * ddxV[i] + dxV[i-1]
             dxP[i] = self.ts * ddxP[i] + dxP[i-1]
@@ -127,7 +202,8 @@ class Simulation:
             xP[i] = self.ts * dxP[i] + xP[i-1]
 
 
-        self.out = {'t': t, 'xV': xV, 'dxV': dxV, 'ddxV': ddxV, 'xP': xP, 'dxP': dxP, 'ddxP': ddxP}
+        self.out = {'t': t, 'xV': xV, 'dxV': dxV, 'ddxV': ddxV, 'xP': xP, 'dxP': dxP, 'ddxP': ddxP,
+        'pedestrianState': pedestrianState, 'vehicleState': vehicleState, 'dxVdes': dxVdes}
 
         return self.out
 
@@ -141,8 +217,8 @@ class Simulation:
         xV = self.out["xV"]
 
         #plot vehicle
-        vehicle    = patches.Rectangle((0, 0), 0, 0, fc='y')
-        pedestrian = patches.Rectangle((0, 0), 0, 0, fc='g')
+        vehicle    = patches.Rectangle((0, 0), 0, 0, fc='k')
+        pedestrian = patches.Rectangle((0, 0), 0, 0, fc='r')
         
         vehicleX = self.vehicle.lane * self.road.laneWidth + self.road.laneWidth / 2 - self.vehicle.width/2
         pedestrianY = self.crosswalk.start[1]
